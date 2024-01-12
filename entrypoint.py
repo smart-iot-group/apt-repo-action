fef0433c88d1e0b1f142e5f0213dae9ae52830fa
trisjakobismart <130749762+trisjakobismart@users.noreply.github.com>
2024-01-12 14:00:24 +0100

    Update entrypoint.py to update a remote apt repo, not github pages anymore

--- entrypoint.py
+++ entrypoint.py
@@ -2,12 +2,27 @@ import os
 import sys
 import logging
 import gnupg
-import git
-import shutil
-import re
-import json
 from debian.debfile import DebFile
 from key import detectPublicKey, importPrivateKey
+import paramiko
+
+def transfer_files_over_ssh(local_dir, remote_dir, hostname, username, password=None, ssh_key=None):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    if ssh_key:
+        ssh.connect(hostname, username=username, key_filename=ssh_key)
+    else:
+        ssh.connect(hostname, username=username, password=password)
+
+    sftp = ssh.open_sftp()
+    for file in os.listdir(local_dir):
+        local_path = os.path.join(local_dir, file)
+        remote_path = os.path.join(remote_dir, file)
+        sftp.put(local_path, remote_path)
+    
+    sftp.close()
+    ssh.close()
 
 debug = os.environ.get('INPUT_DEBUG', False)
 
@@ -19,17 +34,15 @@ else:
 if __name__ == '__main__':
     logging.info('-- Parsing input --')
 
-    github_token = os.environ.get('INPUT_GITHUB_TOKEN')
     supported_arch = os.environ.get('INPUT_REPO_SUPPORTED_ARCH')
     supported_version = os.environ.get('INPUT_REPO_SUPPORTED_VERSION')
     deb_file_path = os.environ.get('INPUT_FILE')
     deb_file_target_version = os.environ.get('INPUT_FILE_TARGET_VERSION')
     github_repo = os.environ.get('GITHUB_REPOSITORY')
 
-    gh_branch = os.environ.get('INPUT_PAGE_BRANCH', 'gh-pages')
     apt_folder = os.environ.get('INPUT_REPO_FOLDER', 'repo')
 
-    if None in (github_token, supported_arch, supported_version, deb_file_path):
+    if None in (supported_arch, supported_version, deb_file_path):
         logging.error('Required key is missing')
         sys.exit(1)
 
@@ -51,84 +64,14 @@ if __name__ == '__main__':
     key_private = os.environ.get('INPUT_PRIVATE_KEY')
     key_passphrase = os.environ.get('INPUT_KEY_PASSPHRASE')
 
-    logging.debug(github_token)
-    logging.debug(supported_arch_list)
-    logging.debug(supported_version_list)
-
     logging.info('-- Done parsing input --')
-
-    # Clone repo
-
-    logging.info('-- Cloning current Github page --')
-
-    github_user = github_repo.split('/')[0]
-    github_slug = github_repo.split('/')[1]
-
-    if os.path.exists(github_slug):
-        shutil.rmtree(github_slug)
-
-    git_repo = git.Repo.clone_from(
-        'https://{}@github.com/{}.git'.format(github_token, github_repo),
-        github_slug,
-    )
-
-    git_refs = git_repo.remotes.origin.refs
-    git_refs_name = list(map(lambda x: str(x).split('/')[-1], git_refs))
-
-    logging.debug(git_refs_name)
-
-    if gh_branch not in git_refs_name:
-        git_repo.git.checkout(b=gh_branch)
-    else:
-        git_repo.git.checkout(gh_branch)
-
-    # Generate metadata
-    logging.debug("cwd : {}".format(os.getcwd()))
-    logging.debug(os.listdir())
-
-    deb_file_handle = DebFile(filename=deb_file_path)
-    deb_file_control = deb_file_handle.debcontrol()
-
-    current_metadata = {
-        'format_version': 1,
-        'sw_version': deb_file_control['Version'],
-        'sw_architecture': deb_file_control['Architecture'],
-        'linux_version': deb_file_version
-    }
-
-    current_metadata_str = json.dumps(current_metadata)
-    logging.debug('Metadata {}'.format(current_metadata_str))
-
-    # Get metadata
-    all_commit = git_repo.iter_commits(gh_branch)
-    all_apt_action_commit = list(filter(lambda x: (x.message[:12] == '[apt-action]'), all_commit))
-    apt_action_metadata_str = list(
-        map(
-            lambda x: re.findall('apt-action-metadata({.+})$', x.message),
-            all_apt_action_commit,
-        )
-    )
-    apt_action_valid_metadata_str = list(filter(lambda x: len(x) > 0, apt_action_metadata_str))
-    apt_action_metadata = list(map(lambda x: json.loads(x[0]), apt_action_valid_metadata_str))
-
-    logging.debug(all_apt_action_commit)
-    logging.debug(apt_action_valid_metadata_str)
-
-    for check_metadata in apt_action_metadata:
-        if (check_metadata == current_metadata):
-            logging.info('Loop detected, exiting')
-            sys.exit(0)
-
-    logging.info('-- Done cloning current Github page --')
 
     # Prepare key
 
     logging.info('-- Importing key --')
 
-    key_dir = os.path.join(github_slug, 'public.key')
     gpg = gnupg.GPG()
-
-    detectPublicKey(gpg, key_dir, key_public)
+    detectPublicKey(gpg, key_public)
     private_key_id = importPrivateKey(gpg, key_private)
 
     logging.info('-- Done importing key --')
@@ -137,13 +80,12 @@ if __name__ == '__main__':
 
     logging.info('-- Preparing repo directory --')
 
-    apt_dir = os.path.join(github_slug, apt_folder)
+    apt_dir = os.path.join(os.getcwd(), apt_folder)
     apt_conf_dir = os.path.join(apt_dir, 'conf')
 
     if not os.path.isdir(apt_dir):
         logging.info('Existing repo not detected, creating new repo')
-        os.mkdir(apt_dir)
-        os.mkdir(apt_conf_dir)
+        os.makedirs(apt_conf_dir)
 
     logging.debug('Creating repo config')
 
@@ -162,11 +104,6 @@ if __name__ == '__main__':
 
     logging.info('-- Adding package to repo --')
 
-    logging.info(apt_dir)
-    logging.info(deb_file_path)
-    logging.info(deb_file_version)
-
-
     logging.info('Adding {}'.format(deb_file_path))
     os.system(
         'reprepro -S utils -P important -b {} --export=silent-never includedeb {} {}'.format(
@@ -184,18 +121,15 @@ if __name__ == '__main__':
 
     logging.info('-- Done adding package to repo --')
 
-    # Commiting and push changes
+    # SSH Transfer
 
-    logging.info('-- Saving changes --')
+    logging.info('-- Transferring files over SSH --')
 
-    git_repo.config_writer().set_value(
-        'user', 'email', '{}@users.noreply.github.com'.format(github_user)
-    )
+    ssh_hostname = 'your_ssh_server'
+    ssh_username = 'your_username'
+    ssh_password = 'your_password'  # or use ssh_key='path_to_your_ssh_key'
+    remote_dir = 'path_to_remote_dir'
 
-    git_repo.git.add('*')
-    git_repo.index.commit(
-        '[apt-action] Update apt repo\n\n\napt-action-metadata{}'.format(current_metadata_str)
-    )
-    git_repo.git.push('--set-upstream', 'origin', gh_branch)
+    transfer_files_over_ssh(apt_dir, remote_dir, ssh_hostname, ssh_username, ssh_password)
 
-    logging.info('-- Done saving changes --')
+    logging.info('-- Done transferring files --')
