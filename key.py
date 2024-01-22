300aafa3d90c34df070867db62c6812a9e71d85f
trisjakobismart <130749762+trisjakobismart@users.noreply.github.com>
2024-01-22 21:16:17 +0100

    Update key.py

--- key.py
+++ key.py
@@ -21,11 +21,6 @@ def detectPublicKey(key_dir):
     gpg = gnupg.GPG(options=['--yes', '--always-trust'])
     public_import_result = gpg.import_keys(pub_key)
 
-    if not public_import_result.fingerprints or len(public_import_result.fingerprints) != 1:
-        logging.error('Invalid public key provided, please provide 1 valid key')
-        logging.error('GPG error: ' + public_import_result.stderr)
-        sys.exit(1)
-
     logging.info('Public key valid')
 
 def importPrivateKey(sign_key):
@@ -34,21 +29,6 @@ def importPrivateKey(sign_key):
 
     gpg = gnupg.GPG(options=['--yes', '--always-trust'])
     private_import_result = gpg.import_keys(sign_key)
-
-    # Check if the import was successful
-    if not private_import_result.fingerprints or len(private_import_result.fingerprints) != 1:
-        logging.error('Invalid private key provided, please provide 1 valid key')
-
-        # Log the error details from GPG without revealing the key
-        error_details = private_import_result.stderr.strip()
-        logging.error('GPG error details: ' + error_details)
-
-        preview_length = 50  # Number of characters to preview
-        key_preview = sign_key[:preview_length] + '...' if sign_key else 'None'
-        logging.error(f'Private key preview (first {preview_length} chars): {key_preview}')
-
-
-        sys.exit(1)
 
     private_key_id = private_import_result.fingerprints[0]
     logging.info('Private key valid')
