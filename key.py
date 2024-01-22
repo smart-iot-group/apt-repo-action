a85b4bbf2996b02d2102993be5e2bbfd8a36377f
trisjakobismart <130749762+trisjakobismart@users.noreply.github.com>
2024-01-22 19:10:48 +0100

    Switch to python-gnupg

--- key.py
+++ key.py
@@ -1,20 +1,16 @@
 import logging
 import os
 import sys
-import gnupg
+import gnupg  # Make sure this is python-gnupg
 
-def detectPublicKey(gpg, key_dir):
+def detectPublicKey(key_dir):
     logging.info('Detecting public key')
 
     public_key_path = os.path.join(key_dir, 'public.key')
-    logging.debug('Looking for public key at {}'.format(public_key_path))
+    logging.debug(f'Looking for public key at {public_key_path}')
 
-    key_exists = os.path.isfile(public_key_path)
-
-    logging.debug('Existing public key file exists? {}'.format(key_exists))
-
-    if not key_exists:
-        logging.error('Public key file not found at {}'.format(public_key_path))
+    if not os.path.isfile(public_key_path):
+        logging.error(f'Public key file not found at {public_key_path}')
         sys.exit(1)
 
     with open(public_key_path, 'r') as key_file:
@@ -22,34 +18,28 @@ def detectPublicKey(gpg, key_dir):
 
     logging.debug('Trying to import key')
     
-    gpg = gnupg.GPG(use_agent=True, options=['--batch', '--pinentry-mode', 'loopback'])
-
+    gpg = gnupg.GPG()
     public_import_result = gpg.import_keys(pub_key)
-    logging.debug(public_import_result)
 
-    if len(public_import_result.fingerprints) != 1:
+    if not public_import_result.fingerprints or len(public_import_result.fingerprints) != 1:
         logging.error('Invalid public key provided, please provide 1 valid key')
         sys.exit(1)
 
-
     logging.info('Public key valid')
 
-
-def importPrivateKey(gpg, sign_key):
+def importPrivateKey(sign_key):
     logging.info('Importing private key')
 
-    gpg = gnupg.GPG(use_agent=True, options=['--batch', '--pinentry-mode', 'loopback'])
-
+    gpg = gnupg.GPG()
     private_import_result = gpg.import_keys(sign_key)
 
-    # Check if exactly one private key was imported
-    if len(private_import_result.fingerprints) != 1:
+    if not private_import_result.fingerprints or len(private_import_result.fingerprints) != 1:
         logging.error('Invalid private key provided, please provide 1 valid key')
         sys.exit(1)
 
     private_key_id = private_import_result.fingerprints[0]
     logging.info('Private key valid')
-    logging.debug('Key id: {}'.format(private_key_id))
+    logging.debug(f'Key id: {private_key_id}')
 
     logging.info('-- Done importing key --')
 
